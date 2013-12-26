3c7f668f012e5ab1791d23d72d561546982e0b5c
Carlos Espejo <carlosespejo@gmail.com>
2013-12-25 22:07:06 -0500

    currently working the test data

--- spec/rsync_spec.rb
+++ spec/rsync_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
+require 'fileutils'
+require 'tmpdir'
 
 describe Rsync do
 #Time.now.strftime "%Y-%m-%d-%H%M%S"
-  
+  before do
+    create_root_dir
+  end
+
   it "should find the latest snapshot" do
     Rsync.new("","").last_snapshot.must_equal ""
   end
 
-  def temp_files
-    
+  after do
+    FileUtils.rm_r @temp_dir
   end
-  
+
+  def temp_files
+
+  end
+
+  def create_root_dir
+    @temp_dir = Dir.mktmpdir('test_data', File.expand_path('./', File.dirname(__FILE__)))
+  end
+
 end
